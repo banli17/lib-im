c11c23d1fc6c4c76323878afd91342890b0c7ce0
banli17 <867889876@qq.com>
2023-02-24 22:57:28 +0800

    update

--- Im.h
+++ Im.h
@@ -8,15 +8,134 @@ using namespace Magick;
 #ifndef TEST_IM_IM_H
 #define TEST_IM_IM_H
 
-class Im {
-public:
-    int getMetaData();
-
-    Im crop(string src, string dist);
-
-private:
-    string src;
-    Image image;
+struct Layer {
+  string type;
+  bool required; // 必填项
+  string value;
+  signed short int width;
+  signed short int height;
+  signed short int left;
+  signed short int top;
+  signed short int rotate; // 旋转角度
+  bool grayscale; // 灰度
+  bool flip;
+  bool flop;
+  float blur; // 模糊
+  int opacity;
+  bool resize;
+  double perspective[];
 };
 
+template<class T>
+int getlength(T &array) {
+  return (sizeof(array) / sizeof(array[0]));
+}
+// 图层转图片
+Image *layer2png(Image *image, Layer layer) {
+  if (!layer.perspective[15]) {
+    size_t numberOfArguments = 16;
+    double *listOfArguments = new double[numberOfArguments];//;layer.perspective;
+    listOfArguments[0] = 0.0;
+    listOfArguments[1] = 0.0;
+    listOfArguments[2] = 0.0;
+    listOfArguments[3] = 0.0;
+    listOfArguments[4] = 500;
+    listOfArguments[5] = 0.0;
+    listOfArguments[6] = 500;
+    listOfArguments[7] = 100;
+    listOfArguments[8] = 0;
+    listOfArguments[9] = 400;
+    listOfArguments[10] = 0;
+    listOfArguments[11] = 300;
+    listOfArguments[12] = 500;
+    listOfArguments[13] = 400;
+    listOfArguments[14] = 500;
+    listOfArguments[15] = 500;
+    image->backgroundColor(Color());
+    image->distort(MagickCore::PerspectiveDistortion,
+                   numberOfArguments, listOfArguments,
+                   MagickCore::MagickFalse);
+  }
+
+  // 变灰
+  if (layer.grayscale) {
+    image->type(GrayscaleType);
+  }
+  if (layer.flip) {
+    image->flip();
+  }
+  if (layer.flop) {
+    image->flop();
+  }
+
+  if (layer.rotate > 0) {
+    image->backgroundColor(Color("#00000000"));
+    image->rotate(layer.rotate);
+  }
+
+  if (isnumber(layer.opacity)) {
+    image->matteColor(Magick::Color("red"));
+  }
+  if (layer.resize) {
+    string wh = to_string(layer.width) + "x" +
+        to_string(layer.height);
+    string g = wh + "+0+0" + ">^";
+    cout << g << endl;
+    image->resize(Geometry(g));
+    image->extent(wh, CenterGravity);
+  }
+
+  if (layer.blur) {
+    image->blur(0, layer.blur);
+  }
+
+  return image;
+}
+
+Image *text2imageLayer() {
+
+}
+
+void initLayer(Layer *layer) {
+  if (!layer->left) layer->left = 0;
+  if (!layer->top) layer->top = 0;
+}
+
+int process(Layer *layer[], string output) {
+  InitializeMagick(NULL);
+
+  Image bg("300x300", "green");
+  try {
+    Image *images[] = {};
+    for (int i = 0; i < 1; i++) {
+      initLayer(layer[i]);
+
+      if (layer[i]->type == "image") {
+        images[i] = new Image(layer[i]->value);
+      } else if (layer[i]->type == "sharp") {
+        images[i] = new Image(Geometry(layer[i]->width, layer[i]->height),
+                              Color("#ffffff00"));
+      } else if (layer[i]->type == "text") {
+        images[i] = new Image(Geometry(layer[i]->width, layer[i]->height),
+                              Color("#ffffffff"));
+        images[i]->font("/Users/banli/Desktop/lib-im/汉仪旗黑-55S.otf");
+        images[i]->fontPointsize(30);
+//        images[i]->fillColor(Color("firebrick"));
+//        images[i]->strokeColor(Color("red"));
+        images[i]->draw(Magick::DrawableText(100, 100, "朋友们"));
+      }
+
+      layer2png(images[i], *layer[i]);
+      bg.composite(*images[i],
+                   Geometry(layer[i]->width, layer[i]->height, layer[i]->left, layer[i]->top),
+                   InCompositeOp);
+    }
+    bg.compressType(MagickCore::WebPCompression);
+    bg.write(output);
+  } catch (Exception &error_) {
+    cout << "Caught exception: " << error_.what() << endl;
+    return 1;
+  }
+  return 0;
+}
 #endif //TEST_IM_IM_H
--- main.cpp
+++ main.cpp
@@ -1,13 +1,51 @@
+#include <Magick++.h>
 #include <iostream>
+#include <string>
 #include "Im.h"
-
 using namespace std;
+using namespace Magick;
 
-int main() {
-    Im im;
-    im.crop(
-            "/Users/banli/Desktop/course/course-cpp/test-im/a.jpeg",
-            "out.png"
-    );
-    return 0;
+int main(int argc, char **argv) {
+  InitializeMagick(NULL);
+  string input = "/Users/banli/Desktop/lib-im/a.jpeg";
+  string output = "/Users/banli/Desktop/lib-im/out.png";
+//  process(input, output);
+  try {
+    cout << "hi0" << endl;
+    Layer l1 = {type : "image", value: input, blur:6,
+        resize: false, width: 100, height: 200};
+    cout << "hi1" << endl;
+    Image *images[] = {};
+    Layer l2 = {
+        value: "/Users/banli/Desktop/lib-im/b.jpg",
+        width: 50, height: 80,
+        left: 30, top: 100,
+        type:"sharp"
+    };
+
+    Layer l3 = {
+        value: "hello world",
+        width: 200, height: 200,
+        type:"text"
+    };
+    Layer *layers[] = {&l3};
+
+    process(layers, output);
+
+//    Image image;
+//    cout << "hi2" << endl;
+//    layer2png(&image, l1);
+//    Image image2;
+//    layer2png(&image2, {
+//        value: "/Users/banli/Desktop/lib-im/b.jpg",
+//        width: 50, height: 50,
+//        type:"sharp"
+//    });
+//    composite(&image, &image2);
+//    image.write(output);
+  } catch (Exception &error_) {
+    cout << "Caught exception: " << error_.what() << endl;
+    return 1;
+  }
+  return 0;
 }
